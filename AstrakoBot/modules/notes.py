c1300d3bd9b5946990f81617fedae91577f87417
D Avinash Kumar <56125142+davinash97@users.noreply.github.com>
2020-07-15 15:28:17 +0530

    Update notes.py

--- AstrakoBot/modules/notes.py
+++ AstrakoBot/modules/notes.py
@@ -177,7 +177,7 @@ def list_notes(bot: Bot, update: Update):
 
     msg = "*Notes in chat:*\n"
     for note in note_list:
-        note_name = escape_markdown(f" - `{}`\n".format(note.name.lower))
+        note_name = f"• `#{escape_markdown(note.name.lower())}`\n"
         if len(msg) + len(note_name) > MAX_MESSAGE_LENGTH:
             update.effective_message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
             msg = ""
