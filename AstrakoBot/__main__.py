85fea63f4b2a9487311ac0ff2c0f4795a82c8826
detzcaius <78322507+detzcaius@users.noreply.github.com>
2021-02-01 04:33:58 +0700

    Update __main__.py

--- AstrakoBot/__main__.py
+++ AstrakoBot/__main__.py
@@ -59,8 +59,8 @@ Dibuat khusus untuk memberantas jamet tele kampang yang meresahkan.
 Saya hanya robot tapi juga bisa sange, jangan lupa pap tt ya asu!!
 ━─━─━─━─━─━─━─━─━─━─━
 
-▪**Managed with by:** [KOALA🐨](https://t.me/Detzcaius)
-▪**JOIN MY CH OFFICIAL:** [CTH](http://t.me/caripasangangrup)
+▪**Managed with by:** [PANDA](https://t.me/Detzcaius)
+▪**JOIN MY CH OFFICIAL:** [CP](http://t.me/caripasangangrup)
 
 ━─━─━─━─━─━─━─━─━─━─━
 Tekan /help untuk melihat daftar fitur saya.
