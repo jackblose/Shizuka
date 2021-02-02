f69387407bb6a9f9d11cf2598a50dd9918818299
detzcaius <78322507+detzcaius@users.noreply.github.com>
2021-02-03 05:28:01 +0700

    Update afk.py

--- AstrakoBot/modules/afk.py
+++ AstrakoBot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} NGENTOT DULU BOS!{}".format(
+        update.effective_message.reply_text("{} SEDANG MELAKUKAN SESUATU!{}".format(
             fname, notice))
     except BadRequest:
         pass
