17e9733876c0a252f24ea5464d73376309c2f05b
Koala <72527434+ManusiaRakitan@users.noreply.github.com>
2021-03-03 21:59:27 +0800

    Update afk.py

--- AstrakoBot/modules/afk.py
+++ AstrakoBot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} SEDANG MELAKUKAN SESUATU!{}".format(
+        update.effective_message.reply_text("{} ngentot dulu gaesss!{}".format(
             fname, notice))
     except BadRequest:
         pass
