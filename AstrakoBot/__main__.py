c9016a01153e9a9eea0776e2237bf9eaa7ace698
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-04-11 19:01:08 +0700

    Update __main__.py

--- AstrakoBot/__main__.py
+++ AstrakoBot/__main__.py
@@ -86,7 +86,7 @@ Dan berikut ini:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-ASTRAKOBOT_IMG = "https://i.ibb.co/MyHhdqR/Pics-Art-04-07-06-23-28.jpg"
+ASTRAKOBOT_IMG = "https://i.ibb.co/5cfkWps/Pics-Art-12-27-05-26-40.jpg"
 
 DONATE_STRING = """Silahkan Donasi Ke sini 
 biar gw bisa update sistem bot nya lagi ya asu.. 
