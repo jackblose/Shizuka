96f181c3c8c1bac8351ec7c1b86857c85dd9a975
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-04-12 09:42:18 +0700

    Update fun_strings.py

--- AstrakoBot/modules/fun_strings.py
+++ AstrakoBot/modules/fun_strings.py
@@ -27,8 +27,8 @@ GIFS = [
 ]
 
 SLAP_SAITAMA_TEMPLATES = (
-    "Slap me one more time and I'll mute you.",
-    "Stop slapping me. REEEEEEEEEEEEEE.",
+    "GAUSAH TAMPAR" GUA,GUA MUTE NIH",
+    "STOP NAMPAR GUA. OOOOOASUUUUUU.",
     [
         "I am muting you for a minute.",  # normal reply
         "Stop slapping me just because I can't mute you. REEEEEEEEEE.",  # reply to admin
