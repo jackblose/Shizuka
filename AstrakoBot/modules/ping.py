f1b55afcd77ba5f0985438f4720fc7bd55464797
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-04-12 09:14:54 +0700

    Update ping.py

--- AstrakoBot/modules/ping.py
+++ AstrakoBot/modules/ping.py
@@ -80,9 +80,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "PONG!!\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "KONTOL!!\n"
+        "<b>DURASI NGENTOT:</b> <code>{}</code>\n"
+        "<b>BANYAK NYA SPERMA:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML)
 
 
